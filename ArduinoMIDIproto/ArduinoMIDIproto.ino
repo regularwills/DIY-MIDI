7b68dc4e579801f34f927ffb819ccf9043873dc9
Will May <willmay009@gmail.com>
2023-05-13 21:37:28 -0700

    Better comments, better pizza, papa Will

--- ArduinoMIDIproto/ArduinoMIDIproto.ino
+++ ArduinoMIDIproto/ArduinoMIDIproto.ino
@@ -1,37 +1,51 @@
+/*---------------------------------------------------------------------
+  BY: Will May
+
+  DIY MIDI Controller : For use of an Arduino with USB capabilities 
+  to act as a MIDI controller for most audio/ music production programs
+
+  REQUIREMENTS: Arduino Microcontroller board and compatabile USB connector
+                Arduino IDE to upload code and "MIDIUSB" lib installed
+                16 push buttons connected to the corresponding I/O pins below
+                2 Potentiomenter knobs connected to corresponding ANALOG pins
+                Any software that a regular MIDI controller can use
+----------------------------------------------------------------------*/
+
 #include "MIDIUSB.h"
 
-/*Set up for an arduino Leonardo as a midi controller corresponding with the MIDIUSB arduino library*/
+const bool debug = false;
 
-//Global variables related to buttons and corresponding pins
-const int TOTAL_BUTTONS = 16;                               // All the Arduino pins used for buttons, in order.
+//Buttons and corresponding pins
+const int TOTAL_BUTTONS = 16;                              
 const int BUTTONS_PIN[TOTAL_BUTTONS] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,A4,A5};          
-                                                            // ^Every pitch corresponding to every Arduino pin. Each note has an associated numeric pitch (frequency scale).
-
+                                                            
+// Starting pitches realted to each button pin
 const int BUTTONS_PITCH[TOTAL_BUTTONS] = {36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51};
-                                                           // ^Current state of the pressed buttons.
-int currentRead[TOTAL_BUTTONS];                            // Temporary input reads to check against current state.
+                                                          
+// Temporary input reads to check against current state.
+int currentRead[TOTAL_BUTTONS];                            
 int tempRead;
 
 
-//Global variables related to potentiometers and corresponding pins
-const int TOTAL_POTS = 2;                                   // All potentiometers (slide or rotary)
-const int POT_PIN[TOTAL_POTS] = {A0,A1};                    // All potentiometers corresponding to every Arduino pin
+//Potentiometers and corresponding pins (slide or rotary)
+const int TOTAL_POTS = 2;    
+const int POT_PIN[TOTAL_POTS] = {A0,A1};                    //WARNING: Make sure these are ANALOG pins               
 
 int potCState[TOTAL_POTS] = {0};                            // Current state of pot knob
 int potPState[TOTAL_POTS] = {0};                            // Previous state of pot knob
-int potStateDifference = 0;                                 // Difference between the current and previous state of the pot
+int potStateDifference = 0;                                 // Difference between the current and previous states
 
 int midiCState[TOTAL_POTS] = {};                            // Current state of midi value
-int midiPState[TOTAL_POTS] = {0};                            // Previous stae of midi value
+int midiPState[TOTAL_POTS] = {0};                           // Previous stae of midi value
 
-const int TIMEOUT = 300;                                     // Amount of time the potentiometer will be read after it exceeds the varThreshold
-const int varThreshold = 10;                                 // * Threshold for the potentiometer signal variation
+const int TIMEOUT = 300;                                    // Amount of time the potentiometer will be read after it exceeds the varThreshold
+const int varThreshold = 10;                                // * Threshold for the potentiometer signal variation
 boolean potMoving = true;                                   
-unsigned long PTime[TOTAL_POTS] = {0};                       // Previously stored time
-unsigned long timer[TOTAL_POTS] = {0};                       // Stores the time that has elapsed since the timer was reset
+unsigned long PTime[TOTAL_POTS] = {0};                      // Previously stored time
+unsigned long timer[TOTAL_POTS] = {0};                      // Stores the time that has elapsed since the timer was reset
 
 
-// The setup function runs once when you press reset or power the board
+// Install firmware for a reset/new Arduino board
 void setup() {
   // Initialize all the pins as a pull-up input.
   for (int i = 0; i < TOTAL_BUTTONS; i++) {
@@ -39,81 +53,103 @@ void setup() {
   }
 }
 
+// Running firmware perpetually on Arduino board
 void loop() {
   buttons();
   potentiometers();
 }
 
-
-//*********************** BUTTONS *****************************************************
+//***** BUTTONS ************
 void buttons(){
-   for (int i = 0; i < TOTAL_BUTTONS; i++) {            // Get the digital state from the button pin.
+  //Ge the digital state from a button, store that state
+   for (int i = 0; i < TOTAL_BUTTONS; i++) {           
      int buttonState = digitalRead(BUTTONS_PIN[i]);     // In pull-up inputs the button logic is inverted (HIGH is not pressed, LOW is pressed).
-     tempRead = buttonState;                             // Temporarily store the digital state.
+     tempRead = buttonState;                            
   
-     if (currentRead[i] != tempRead) {                   // Continue only if the last state is different to the current state.
-       // See https://www.arduino.cc/en/pmwiki.php?n=Tutorial/Debounce
+    // Continue if the last recorded state is different to current state
+     if (currentRead[i] != tempRead) {                   
+       // Delay for switch bounce  -----> See https://www.arduino.cc/en/pmwiki.php?n=Tutorial/Debounce
        delay(2);
-       int pitch = BUTTONS_PITCH[i];                    // Get the pitch mapped to the pressed button.             
-       currentRead[i] = tempRead;                        // Save the new input state.
+
+       //Get the mapped pitch for this pin and save the new input state
+       int pitch = BUTTONS_PITCH[i];                             
+       currentRead[i] = tempRead;                        
       
-       if (buttonState == LOW) {                         // Execute note on or noted off depending on the button state.
-         noteOn(pitch);                                  // Low equals button is being/has been pressed
+       // Play or stop playing the note based on the digital read
+       if (buttonState == LOW) {                        
+         noteOn(pitch);                   // Button pressed                
        } 
        else {
-         noteOff(pitch);
+         noteOff(pitch);                  // Button not pressed
        }
      }
    }
 }
 
+// Send a midi note
 void noteOn(int pitch) {
   MidiUSB.sendMIDI({0x09, 0x90, pitch, 127});
   MidiUSB.flush();
 }
 
+// Terminate a midi note
 void noteOff(int pitch) {
   MidiUSB.sendMIDI({0x08, 0x80, pitch, 0});
   MidiUSB.flush();
 }
 
-//********************** POTENTIOMETERS ***********************************************
+//********* POTENTIOMETERS ***********
 void potentiometers() {
-  for (int i = 0; i < TOTAL_POTS; i++) {                  // Loops through all the potentiometers
-    potCState[i] = analogRead(POT_PIN[i]);                // Read in pins from the arduino board
-    midiCState[i] = map(potCState[i], 0, 1023, 0, 127);   // Maps the reading of the potCState to a value usable in midi
-    potStateDifference = abs(potCState[i] - potPState[i]);// Calculates the absolute value between the difference 
-                                                          // between the current and previous state of the pot knob
+  // Read in the anaolg pins for pot knobs. 
+  // Grab the abs value of the difference in pot states
+  for (int i = 0; i < TOTAL_POTS; i++) {                  
+    potCState[i] = analogRead(POT_PIN[i]);               
 
-    if (potStateDifference > varThreshold) {              // Opens the gate if the potentiometer variation is greater than the threshold
-      PTime[i] = millis();                                // Stores the previous time
+    // Maps the reading of the potCState to a value usable in midi
+    midiCState[i] = map(potCState[i], 0, 1023, 0, 127);   
+    potStateDifference = abs(potCState[i] - potPState[i]);
+
+    // Opens gate if the pot variation is greater than the threshold, 
+    // stores the previous time                              
+    if (potStateDifference > varThreshold) {              
+      PTime[i] = millis();                                
     }
+    
+    // Reset timer to 0ms
+    timer[i] = millis() - PTime[i];                       
 
-    timer[i] = millis() - PTime[i];                       // Resets the timer to 0ms
-
-    if (timer[i] < TIMEOUT) {                             // If the timer is less than the maximum allowed time
-      potMoving = true;                                   // then the potentiometer is still moving
+    // If the timer is less than max time, then the pot knob is still moving
+    if (timer[i] < TIMEOUT) {                             
+      potMoving = true;                                   
     }
     else {  
       potMoving = false;
     }
 
-    if (potMoving == true) {                              // If the potentiometer is still moving, send the change control
+  // If the potentiometer is still moving, send the change control
+    if (potMoving == true) {                              
       if (midiPState[i] != midiCState[i]) {
 
         controlChange(1, 1 + i, midiCState[i]);           // (MIDI channel to be used, lowest MIDI CC number,  CC value)
         MidiUSB.flush();
-        Serial.print("Pot: ");
-        Serial.print(i);
-        Serial.print(" ");
-        Serial.println(midiCState[i]);
-        potPState[i] = potCState[i];                      // Stores the current reading of the potentiometer to compare with the next
+        
+        // **** FOR DEBUGGING POTENTIOMETER ISSUES ****
+        if (debug){
+          Serial.print("Pot: ");                         
+          Serial.print(i);                               
+          Serial.print(" ");
+          Serial.println(midiCState[i]);
+        }
+
+        // Stores the current reading of the potentiometer to compare with the next
+        potPState[i] = potCState[i];                      
         midiPState[i] = midiCState[i];
       }
     }
   }
 }
 
+// Convert the pot state to a MIDI control change. (Should also be included with MIDIUSB.h) 
 void controlChange(int channel, int control, int value){
   midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
   MidiUSB.sendMIDI(event);
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# DIY MIDI Controller
+# DIY MIDI Controller 
+# By Will May (2022)
 Generalized source code used to upload to any arduino board with USB capabilities. Dependent on the Arduio IDE MIDIUSB library.
 This was used for a arduino leonardo but can be used for a micro and other boards as well. 
